7d5f1eef7e060e83d660d9fc052cf658bcbaa9e1
voidStellar14 <80975993+voidStellar14@users.noreply.github.com>
2022-12-19 10:29:25 -0500

    Add files via upload

---
+++ jebaited/src/Jebaited.java
@@ -0,0 +1,26 @@
+
+public class Jebaited 
+{
+	/**
+     * @author voidStellar14
+     * @version 1.1
+     */
+    public static void main(String[] args) 
+	{
+        System.out.println("you just got jebaited");
+    	/**
+         * prints "jebaited" infinitely
+         */
+        try {
+            Thread.sleep (5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        for(int i = 0; i > -1; i++)
+        {
+            System.out.println("jjjjjebaited");
+           
+        }
+    }
+}
